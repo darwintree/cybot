0f2765beccae0dbce8e22c933b607eecc04c9d56
unknown <daerwendeshu@outlook.com>
2019-12-28 12:29:43 +0800

    更改监听端口

--- cybot.py
+++ cybot.py
@@ -7,4 +7,4 @@ if __name__ == '__main__':
     nonebot.init(config)
     nonebot.load_plugins(path.join(path.dirname(__file__), 'cy', 'plugins'),
         'cy.plugins')
-    nonebot.run(host='127.0.0.1', port=8080)
+    nonebot.run(host='0.0.0.0', port=8080)
